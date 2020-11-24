d687fc8ca81429ee791ee724850b91f5d029a88f
Diego Azevedo <dapptn@hotmail.com>
2020-11-25 03:31:15 +1100

    list of interests cleaned up

--- js/user-dialog.js
+++ js/user-dialog.js
@@ -3,6 +3,7 @@ $(document).on("pagebeforeshow", "#user-dialog", function (event, data) {
   $(".user-mail", $(this)).text(USER_REGISTRATION.email);
   $(".user-camera", $(this)).text(USER_REGISTRATION.camera);
 
+  $(".interest-items", $(this)).html("");
   USER_REGISTRATION.interests.forEach((interest) => {
     $(".interest-items", $(this)).append(
       "<li><strong>" + interest + "</strong></li>"
